f5fdca1b73dabf5f26d76999b751757566c5dddc
tomaszzawada <tomasz.zawada88@gmail.com>
2019-05-25 19:36:00 +0200

    improved formating

--- js/main.js
+++ js/main.js
@@ -1,39 +1,21 @@
-const listName = document.querySelector('p.listName');
-const inputListName = document.querySelector('input.inputListName');
-const btnListName = document.querySelector('button.btnListName');
-const inputAdd = document.querySelector('input.inputAdd');
-const btnAdd = document.querySelector('button.btnAdd');
+const listName = document.querySelector("p.listName");
+const inputListName = document.querySelector("input.inputListName");
+const btnListName = document.querySelector("button.btnListName");
 
 // Change List Name
 function changeListName() {
-    const value = inputListName.value;
-    if (value.length > 0) {
-        listName.innerHTML = value + ":";
-    }
+  const nameValue = inputListName.value;
+  if (nameValue.length > 0) {
+    listName.innerHTML = nameValue + ":";
+  }
 }
 
-btnListName.addEventListener('click', () => {
+btnListName.addEventListener("click", () => {
+  changeListName();
+});
+
+inputListName.addEventListener("keyup", e => {
+  if (e.keyCode == 13) {
     changeListName();
-});
-
-inputListName.addEventListener('keyup', (e) => {
-    if(e.keyCode == 13) {
-        changeListName();
-    }
-});
-
-// Add Items to list
-function addItem() {
-    let ul = document.getElementsByTagName('ul')[0];
-    let li = document.createElement('li');
-    const value = inputAdd.value;
-    if (value.length > 0) {
-        li.textContent = value;
-        ul.appendChild(li);
-        inputAdd.value = '';
-    }
-}
-
-btnAdd.addEventListener('click', ()=> {
-    addItem();
+  }
 });
