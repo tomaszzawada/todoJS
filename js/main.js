ae173d91f647a8d31502cbc430fecae193c6955c
tomaszzawada <tomasz.zawada88@gmail.com>
2019-05-25 20:09:47 +0200

    remove item from list feature added

--- js/main.js
+++ js/main.js
@@ -1,45 +1,67 @@
-const listName = document.querySelector('p.listName');
-const inputListName = document.querySelector('input.inputListName');
-const btnListName = document.querySelector('button.btnListName');
-const inputAdd = document.querySelector('input.inputAdd');
-const btnAdd = document.querySelector('button.btnAdd');
+const listName = document.querySelector("p.listName");
+const inputListName = document.querySelector("input.inputListName");
+const btnListName = document.querySelector("button.btnListName");
+const inputAdd = document.querySelector("input.inputAdd");
+const btnAdd = document.querySelector("button.btnAdd");
+const listDiv = document.querySelector(".list");
+const listUl = listDiv.querySelector("ul");
 
 // Change List Name
 function changeListName() {
-    const value = inputListName.value;
-    if (value.length > 0) {
-        listName.innerHTML = value + ":";
-    }
+  const value = inputListName.value;
+  if (value.length > 0) {
+    listName.innerHTML = value + ":";
+  }
 }
 
-btnListName.addEventListener('click', () => {
-    changeListName();
+btnListName.addEventListener("click", () => {
+  changeListName();
 });
 
-inputListName.addEventListener('keyup', (e) => {
-    if(e.keyCode == 13) {
-        changeListName();
-    }
+inputListName.addEventListener("keyup", e => {
+  if (e.keyCode == 13) {
+    changeListName();
+  }
 });
 
 // Add Items to list
 function addItem() {
-    let ul = document.getElementsByTagName('ul')[0];
-    let li = document.createElement('li');
-    const value = inputAdd.value;
-    if (value.length > 0) {
-        li.textContent = value;
-        ul.appendChild(li);
-        inputAdd.value = '';
-    }
+  let ul = document.getElementsByTagName("ul")[0];
+  let li = document.createElement("li");
+  const value = inputAdd.value;
+  if (value.length > 0) {
+    li.textContent = value;
+    addLiBtn(li);
+    ul.appendChild(li);
+    inputAdd.value = "";
+  }
 }
 
-btnAdd.addEventListener('click', () => {
-    addItem();
+btnAdd.addEventListener("click", () => {
+  addItem();
 });
 
-inputAdd.addEventListener('keyup', (e) => {
-    if(e.keyCode == 13) {
-        addItem();
+inputAdd.addEventListener("keyup", e => {
+  if (e.keyCode == 13) {
+    addItem();
+  }
+});
+
+// Add remove button to list items
+function addLiBtn(li) {
+  let remove = document.createElement("button");
+  remove.className = "remove";
+  remove.textContent = "x";
+  li.appendChild(remove);
+}
+
+// Removing Item from List
+listUl.addEventListener("click", e => {
+  if (e.target.tagName == "BUTTON") {
+    if (e.target.className == "remove") {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
     }
+  }
 });
